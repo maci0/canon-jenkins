03e623a96af46184a1372c02fd08ef54c194ecb8
Marcel Wysocki <maci.stgn@gmail.com>
2014-05-08 18:42:09 +0200

    Update app.js

--- app.js
+++ app.js
@@ -19,12 +19,6 @@ document.observe("dom:loaded", function () {
     $('main-table').wrap('div', { 'id': 'main-table-wrap' });
     // Clear style attribute for main table
     $('main-table').setStyle({background:"none"});
-    // Create table row with Rackspace logo and search field cells
-    var logoRow = new Element('tr').update('<td colspan="2" id="rackspace-logo"></td><td id="search-wrap"></td>');
-    // Prepend logo row to main table body
-    $$('#main-table tbody :first-child')[0].insert({
-        before: logoRow
-    });
     // Copy search form into search-wrap
     $('search-wrap').update($$('#top-panel table td:nth-child(2)')[0].innerHTML);
     // Grab searchbox
